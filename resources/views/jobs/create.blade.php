fdbdd72dae60fcab050d492f4b7d75e7b3ee09b4
mennaabdelelhady <menamahmod660@gmail.com>
2025-07-14 01:43:21 +0300

    login and register

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -10,20 +10,30 @@
                 <p class="mt-1 text-sm/6 text-gray-600">we just need handful details.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+                    <x-form-field>
+                        <x-form-label for="title">Title</x-form-label>
+
                         <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input type="text" name="title" id="title"
-                                    class="block min-w-0 grow py-1.5 px-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                    placeholder="shift Leader" required/>
-                            </div>
+                            <x-form-input name="title" id="title" placeholder="ceo" ></x-form-input>
+                            <x-form-error name="title"></x-form-error>
                         </div>
-                        @error('title')
-                            <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
-                        @enderror
-                    </div>
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for="title">company</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input name="company" id="location" placeholder="Egypt" />
+                            <x-form-error name="company"></x-form-error>
+                        </div>
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for="title">company</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input name="company" id="location" placeholder="Egypt" />
+                            <x-form-error name="company"></x-form-error>
+                        </div>
+                    </x-form-field>
 
                     <div class="sm:col-span-4">
                         <label for="company" class="block text-sm/6 font-medium text-gray-900">Company</label>
@@ -32,7 +42,7 @@
                                 class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input type="text" name="company" id="company"
                                     class="block min-w-0 grow py-1.5 px-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                    placeholder="$50,000 Per Year" required />
+                                    placeholder="$50,000 Per Year" />
                             </div>
                         </div>
                     </div>
@@ -45,8 +55,7 @@
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
             <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-            <button type="submit"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            <x-form-button>Save</x-form-button>
         </div>
     </form>
 
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use App\Models\Job;
 
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 Route::resource('jobs', JobController::class);
+
+//auth
+Route::get('/register',[RegisteredUserController::class, 'create']);
+Route::post('/register',[RegisteredUserController::class, 'store']);
+
+Route::get('/login',[SessionController::class, 'create']);
+Route::post('/login',[SessionController::class, 'store']);
+
+
